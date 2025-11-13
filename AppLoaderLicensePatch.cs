402bff8c87ef54e1abd78b0ac5857e21c24a1040
Bora <173380240+tyy-dev@users.noreply.github.com>
2025-11-13 13:59:46 +0100

    Add files via upload

---
+++ AppLoaderLicensePatch.cs
@@ -0,0 +1,38 @@
+﻿using Autodesk.Revit.UI;
+using HarmonyLib; 
+using ricaun.AppLoader; // copy local = false
+using System.Reflection;
+
+namespace RevitHarmony;
+
+public static class AppLoaderLicensePatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Api), "IsInDevelop")]
+    [HarmonyPatch(typeof(Api), "ApiCheck")]
+    [HarmonyPatch(typeof(Api), "ApiFull")]
+    private static bool Prefix(ref bool __result)
+    {
+        __result = true;  // override return value
+        return false;
+    }
+
+    public static void ApplyPatch()
+    {
+        Assembly appLoaderAssembly = AppDomain.CurrentDomain.GetAssemblies()
+            .FirstOrDefault(a => a.GetName().Name.Equals("ricaun.AppLoader", StringComparison.OrdinalIgnoreCase));
+
+        if (appLoaderAssembly == null) // sanity, shouldn't happen.
+            return;
+
+        Harmony harmony = new("AppLoaderLicensePatch");
+        harmony.PatchAll(typeof(AppLoaderLicensePatch));
+
+        IEnumerable<MethodBase> patchedMethods = Harmony.GetAllPatchedMethods();
+        if (patchedMethods.Any())
+            TaskDialog.Show("license patch", "Hooked license methods for AppLoader successfully");
+        else
+            TaskDialog.Show("license patch", "Failed to hook license methods for AppLoader");
+
+    }
+}
---
+++ RevitHarmony.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.UI;
+using HarmonyLib;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.AccessControl;
+
+namespace RevitHarmony;
+
+public class PatchAppLoaderLicense : IExternalApplication
+{
+    public Result OnShutdown(UIControlledApplication application) => Result.Succeeded;
+    public Result OnStartup(UIControlledApplication application)
+    {
+        AppDomain.CurrentDomain.AssemblyLoad += (sender, args) =>
+        {
+            if (args.LoadedAssembly.GetName().Name.Equals("ricaun.AppLoader", StringComparison.OrdinalIgnoreCase))
+                AppLoaderLicensePatch.ApplyPatch();
+            
+        };
+        return Result.Succeeded;
+    }
+}
